760d9a69b73c9330ebe54c5c248077f8c43f5248
Zenitsu-god <pranavlande1@gmail.com>
2024-10-26 21:29:44 +0530

    darkmode, container align,navbar, icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import LatestPosts from "@/components/latest-posts";
+import LatestPosts from "@/components/home/latest-posts";
+import { MainNav } from "@/components/home/main-nav";
+import { Container } from "@/components/ui/Container";
 
 export default function Home() {
   return (
-    <div>
-      <LatestPosts />
-    </div>
+    <Container>
+      <MainNav />
+      <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
+        <div>
+          <LatestPosts />
+        </div>
+        <div className="h-dvh">
+          <div>
+            <h1 className="uppercase font-bold mb-4">
+              Top categories
+            </h1>
+            {/* <TopCategories /> */}
+          </div>
+          <div className="sticky top-0 mt-10">
+            <h1 className="uppercase font-bold mb-4">
+              Popular Posts
+            </h1>
+            {/* <PopularPosts /> */}
+          </div>
+        </div>
+      </main>
+    </Container>
   );
 }
---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,8 @@
+import { Command,Rss, type Icon as LucideIcon } from "lucide-react";
+
+export type Icon = typeof LucideIcon;
+
+export const Icons ={
+    logo: Command,
+    rss: Rss,
+}
